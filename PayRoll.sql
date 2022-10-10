e48657c065ea1ab655b8d42b472b6f3c561371a4
Payal Sharma <https://github.com/29payal>
2022-10-10 20:46:16 +0530

    [Payal] UC4-SelectOperation

--- PayRoll.sql
+++ PayRoll.sql
@@ -13,10 +13,12 @@ StartDate varchar(20)
 )
 
 --------UC3 insert data in the table------
-insert into employee_payroll values 
+insert into Employee_payroll values 
 ('payal',25000,'12-08-2019'),
 ('Ritu',23000.20,'22-09-2016'),
 ('Bill',12000.33,'19-11-2018'),
 ('Minu',22500.200,'14-03-2019'),
 ('Charlie',36123,'29-02-2020')
 
+--------------------UC4 Retrive Data into Table---------
+select * from Employee_payroll 
