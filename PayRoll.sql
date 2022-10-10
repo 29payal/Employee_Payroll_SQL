4bef0b0872a400ace4e3859aee8964768b2f3bbe
Payal Sharma <https://github.com/29payal>
2022-10-10 20:36:22 +0530

    [Payal] UC2-CreateTable

--- PayRoll.sql
+++ PayRoll.sql
@@ -2,3 +2,12 @@
 create database Payroll_service
 
 use Payroll_service
+
+------UC2 Create a employee payroll table------
+create table Employee_Payroll
+(
+Id int identity(1,1) primary key,
+Name varchar(20),
+Salary varchar(10),
+StartDate varchar(20)
+)
