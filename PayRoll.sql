6b518ee6963cd0c89dc931aba0e0c7cf42375a07
Payal Sharma <https://github.com/29payal>
2022-10-10 20:42:44 +0530

    [Payal] UC3-InsertData

--- PayRoll.sql
+++ PayRoll.sql
@@ -11,3 +11,12 @@ Name varchar(20),
 Salary varchar(10),
 StartDate varchar(20)
 )
+
+--------UC3 insert data in the table------
+insert into employee_payroll values 
+('payal',25000,'12-08-2019'),
+('Ritu',23000.20,'22-09-2016'),
+('Bill',12000.33,'19-11-2018'),
+('Minu',22500.200,'14-03-2019'),
+('Charlie',36123,'29-02-2020')
+
